6e4ad04ae621ee29a149d3e2d37be851916996a8
Uzair <uzair.az@gmail.com>
2022-05-05 22:11:01 +0300

    buttons with style,
    Rest get for sum not working.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import React, { useEffect, useState, useRef } from "react";
 import './App.css';
 
 
 function App() {
 
-  const [isActive, setIsActive] = useState(false);
 
+  const [isActive, setIsActive] = useState(false);
   const [value, setValue] = useState('');
+  const myContainer = useRef(null);
+  // this.state = {
+  //   result: []
+  // }
+  var sNumbers = "";
+
+  function handleNumberSubmission(e) {
+    if (e.code === "Enter" || e.code === "NumpadEnter") {
+      // concatenate to the numbers string
+      sNumbers += e.target.value + ",";
+      myContainer.current.innertext = sNumbers; // Not working
+      myContainer.current.label.innertext = sNumbers;
+      myContainer.current.text = sNumbers;
+      // clear the text in the field.
+      setValue("");
+      console.log("Enter key was pressed. Run your function.");
+    }
+  }
 
   function handleTextChange(text) {
-    const re = /^[0-9\b]+$/;
+    const re = /^[0-9\b]+$/; // to allow just numeric values
 
     // if value is not blank, then test the regex
     if (re.test(text) || text === '') {
       setValue(text);
     }
-    // if (text === '' || re.test(text.target.value)) {
-    //    this.setState({value: text})
-    // }
-
 
     if (text !== '') {
       setIsActive(true);
@@ -26,24 +41,48 @@ function App() {
       setIsActive(false);
     }
   }
-  return (
-    <div className="App">
-      <header>
-        <p>
-          Sum and Prime
-        </p>
-      </header>
-      <body>
-        <div className="float-label">
-          <input type="text"
-            value={value}
-            onChange={(e) => handleTextChange(e.target.value)} />
 
-          <label className={isActive ? "Active" : ""} htmlFor="email">
-            Enter a number
-          </label>
-        </div>
-      </body>
+  function CheckPrime(text) {
+
+  }
+
+  function calculateSumAndCheckPrime(text) {
+    axios.get('https://localhost:44331/api/MyAPI?action=sumandcheck&numbers=13,12,22,5')
+    .then(response => console.log(response))
+  }
+  return (
+    <div className="App" >
+      <form autoComplete="off">
+        <header>
+          <p>
+            Sum and Prime
+          </p>
+        </header>
+        <body>
+          <div className="float-label">
+            <input type="text"
+              value={value}
+              onChange={(e) => handleTextChange(e.target.value)}
+              onKeyPress={(e) => handleNumberSubmission(e)}
+            />
+
+            <label className={isActive ? "Active" : ""} htmlFor="number">
+              Enter a number
+            </label>
+          </div>
+
+          <div>
+            <button onClick={calculateSumAndCheckPrime}>Calculate sum</button>
+            <button onClick={CheckPrime}>Check if Prime number</button>
+          </div>
+        </body>
+      </form>
+      <div>
+        <label
+          ref={myContainer}
+        >output text
+        </label>
+      </div>
     </div>
   );
 }
