26928978ebe0092826e35b049c0654b991df5a32
Uzair <uzair.az@gmail.com>
2022-05-06 20:53:16 +0300

    Tool tip not working.
    Not hiding when focus is over.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 // import axios from 'axios'
-import React, { useEffect, useState, useRef } from "react";
+import React, { Component, useState, useRef } from "react";
 import './App.css';
-
+import ReactTooltip from "react-tooltip";
+// import Tippy from "@tippy.js/react";
+// import "tippy.js/dist/tippy.css";
 
 function App() {
 
-
+  // can we have one isActive for both text boxes?
   const [isActive, setIsActive] = useState(false);
+  const [isEndpointActive, setIsEndpointActive] = useState(false);
+  const [isPrimeButtonDisabled, setIsPrimeButtonDisabled] = useState(false);
   const [value, setValue] = useState('');
-  const myContainer = useRef(null);
-  // this.state = {
-  //   result: []
-  // }
-  var sNumbers = "";
+  const [endpoint, setEndpoint] = useState('')
+  const [outputText, setOutputText] = useState('')
+  // const [numbers, setNumbers] = useState('')
+  const sNumbers = useRef('');
 
   function handleNumberSubmission(e) {
-    if (e.code === "Enter" || e.code === "NumpadEnter") {
+    if (e.target.value !== '' && (e.code === "Enter" || e.code === "NumpadEnter")) {
       // concatenate to the numbers string
-      sNumbers += e.target.value + ",";
-      myContainer.current.innertext = sNumbers; // Not working
-      myContainer.current.label.innertext = sNumbers;
-      myContainer.current.text = sNumbers;
+      sNumbers.current = sNumbers.current + e.target.value + ",";
+
+      console.log("sNumbers: " + sNumbers.current + " Length: " + sNumbers.current.split(',').length);
+
+      // Disable CheckPrime button if numbers are more than 1.
+      CheckPrimeButton();
       // clear the text in the field.
       setValue("");
-      console.log("Enter key was pressed. Run your function.");
+    }
+  }
+
+  function CheckPrimeButton() {
+    setOutputText(sNumbers.current);
+    if (sNumbers.current.split(',').length === 2) {
+      setIsPrimeButtonDisabled(false);
+    } else {
+      setIsPrimeButtonDisabled(true);
     }
   }
 
@@ -42,45 +55,84 @@ function App() {
     }
   }
 
-  function CheckPrime(text) {
+  function handleEndpointTextChange(text) {
+    setEndpoint(text);
+    if (text !== '') {
+      setIsEndpointActive(true);
+    } else {
+      setIsEndpointActive(false);
+    }
+  }
 
+  function CheckPrime(text) {
+    console.log("CheckPrime, endpoint: " + endpoint);
   }
 
   function calculateSumAndCheckPrime(text) {
-    // axios.get('https://localhost:44331/api/MyAPI?action=sumandcheck&numbers=13,12,22,5')
-    // .then(response => console.log(response))
+    // lets remove the , at the end..
+    sNumbers.current = sNumbers.current.slice(0, -1);
+    console.log("sNumbers before sum: " + sNumbers.current);
+
+    if (endpoint === '') {
+      console.log("No Endpoint given");
+    } else {
+
+      // axios.get(endpoint + '?action=sumandcheck&numbers=' +  sNumbers.current)
+      // .then(response => console.log(response))
+    }
+
+
   }
+
   return (
     <div className="App" >
-      <form autoComplete="off">
-        <header>
-          <p>
-            Sum and Prime
-          </p>
-        </header>
-        <body>
-          <div className="float-label">
-            <input type="text"
-              value={value}
-              onChange={(e) => handleTextChange(e.target.value)}
-              onKeyPress={(e) => handleNumberSubmission(e)}
-            />
+      <header>
+        <p>
+          Sum and Prime
+        </p>
+      </header>
 
-            <label className={isActive ? "Active" : ""} htmlFor="number">
-              Enter a number
-            </label>
-          </div>
+      <div className="float-label">
+        <div className="float-label">
+          <input type="text"
+            value={endpoint}
+            onChange={(e) => handleEndpointTextChange(e.target.value)}
+          />
+          <label className={isEndpointActive ? "Active" : ""} htmlFor="endpoint">
+            Endpoint goes here
+          </label>
+        </div>
+      </div>
+      <div className="float-label">
+        <input type="text"
+          value={value}
+          onChange={(e) => handleTextChange(e.target.value)}
+          onKeyPress={(e) => handleNumberSubmission(e)}
+        />
 
-          <div>
-            <button onClick={calculateSumAndCheckPrime}>Calculate sum</button>
-            <button onClick={CheckPrime}>Check if Prime number</button>
-          </div>
-        </body>
-      </form>
+        <label className={isActive ? "Active" : ""} htmlFor="number">
+          Enter a number
+        </label>
+      </div>
+
+      <div>
+        <button onClick={calculateSumAndCheckPrime}>Calculate sum</button>
+
+        <button
+          data-tip data-for='msgPrime'
+          onClick={CheckPrime}
+          onMouseLeave={() => {
+            console.log("mouse leave")
+          }}
+          className={isPrimeButtonDisabled ? "Disabled" : ""}
+          disabled={isPrimeButtonDisabled}>Check if Prime number</button>
+        <ReactTooltip id='msgPrime' type='error'>
+          <span>Cannot Check for prime number is there are more than 1 numbers entered</span>
+        </ReactTooltip>
+      </div>
       <div>
         <label
-          ref={myContainer}
-        >output text
+          value={outputText}>output text
         </label>
       </div>
     </div>
